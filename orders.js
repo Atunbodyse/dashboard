0eacaa7e5177b6909207e0b51e68b7a3ef71bdb9
Atunbodyse <131750784+Atunbodyse@users.noreply.github.com>
2024-02-17 12:41:40 +0100

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,65 @@
+const Orders  = [
+    {
+        productName: 'Smart watch',
+        productNubmer: '74644',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+
+    },
+    {
+        productName: 'Smart pen',
+        productNubmer: '56444',
+        paymentStatus: 'paid',
+        shipping: 'Delivered'
+
+    },
+    {
+        productName: 'Smart phone',
+        productNubmer: '84644',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+
+    },
+    {
+        productName: 'Smart camera',
+        productNubmer: '78693',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+
+    },
+    {
+        productName: 'Smart Tablet',
+        productNubmer: '8977',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+
+    },
+    {
+        productName: 'Smart 3D glass',
+        productNubmer: '96902',
+        paymentStatus: 'Paid',
+        shipping: 'pending'
+
+    },
+    {
+        productName: 'Smart watch',
+        productNubmer: '74644',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+
+    },
+    {
+        productName: 'Smart Earpuds',
+        productNubmer: '74644',
+        paymentStatus: 'Paid',
+        shipping: 'pending'
+
+    },
+    {
+        productName: 'Smart Drone',
+        productNubmer: '90877',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+
+    },
+]
